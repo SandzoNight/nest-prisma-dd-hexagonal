718ae1c1ce962334ec9496fda559ea32da97cc71
Trisattawat <trisattawat.mekchay@kingpower.com>
2024-07-24 13:33:57 +0700

    add mock method

---
+++ src/test/mock-fn.ts
@@ -0,0 +1,11 @@
+export const mockMemberServices = {
+  findById: jest.fn(),
+  createMember: jest.fn(),
+};
+
+export const mockMemberRepository = {
+  findAll: jest.fn(),
+  findById: jest.fn(),
+  create: jest.fn(),
+  update: jest.fn(),
+};
